b4165ab53e46a385f7a6a5647d2ea6f49da569e0
Rajeev Sahu <rajeevsahu210@gmail.com>
2023-04-13 17:46:26 +0530

    fixed cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://note-app-socket.netlify.app"],
   },
 });
 
